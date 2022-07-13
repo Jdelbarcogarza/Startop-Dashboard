5ac42fc6424082cab4aa5bf8a49dfdbef390cbe4
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-13 17:29:45 -0500

    se creo componente de menu de usuario

---
+++ src/components/popup-account-menu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Paper, Box, Typography, Divider} from '@mui/material'
+
+const accountMenu = () => {
+  return (
+    <div>accountMenu</div>
+  )
+}
+
+export default accountMenu
