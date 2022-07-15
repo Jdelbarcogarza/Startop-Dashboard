629789b50cc7d38615cc63b92ad925db036cca15
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-14 19:22:41 -0500

    diseño de account dropdown preliminar

--- src/components/popup-account-menu.js
+++ src/components/popup-account-menu.js
@@ -1,10 +1,121 @@
 import React from 'react'
-import { Paper, Box, Typography, Divider} from '@mui/material'
+import {
+  Paper, Box, Typography, Divider, Stack, Item
+} from '@mui/material'
 
-const accountMenu = () => {
+
+// imports (account dropdown menu)
+import PaymentOutlinedIcon from '@mui/icons-material/PaymentOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+
+
+// apartados de navegacion de drop down menu
+const optionsWithIcon = [
+  {
+    icon: <PaymentOutlinedIcon />,
+    title: 'Pagos'
+  },
+  {
+    icon: <PersonOutlineIcon />,
+    title: 'Cuenta'
+  },
+  {
+    icon: <SettingsOutlinedIcon />,
+    title: 'Ajustes'
+  },
+]
+
+const optionsNoIcon = [
+  {
+    title: 'Ayuda'
+  },
+  {
+    title: 'Términos y condiciones'
+  },
+
+]
+
+
+const MenuDivider = () => (
+  <Divider sx={{mx: 2, my:1, borderColor: 'primary.main'}} />
+
+)
+
+export const AccountMenu = () => {
+
   return (
-    <div>accountMenu</div>
+    <Paper sx={{ width: '230px', height: 'fit', position: 'absolute', bottom: 80, paddingY: 2 }}>
+      <Stack sx={{ position: 'relative' }}
+        spacing={0}
+      >
+
+        
+          {optionsWithIcon.map((item) => (
+            <Box key={item.title}
+              sx={{
+                display: 'flex',
+                paddingY: 1,
+                paddingX: 2,
+                '&:hover': {
+                  backgroundColor: 'secondary.main'
+                }
+              }}>
+              {item.icon}
+              <Typography variant="body2"
+                color="initial"
+                sx={{ ml: 1 }}
+              >
+                {item.title}
+              </Typography>
+            </Box>
+          )
+
+          )}
+
+              <MenuDivider />
+
+              {optionsNoIcon.map((item) => (
+            <Box key={item.title}
+              sx={{
+                display: 'flex',
+                paddingY: 1,
+                paddingX: 1,
+                '&:hover': {
+                  backgroundColor: 'secondary.main'
+                }
+              }}>
+              <Typography variant="body2"
+                color="initial"
+                sx={{ ml: 1 }}
+              >
+                {item.title}
+              </Typography>
+            </Box>
+          )
+              )}
+
+              <MenuDivider />
+
+
+              <Box
+              sx={{
+                display: 'flex',
+                paddingY: 1,
+                paddingX: 1,
+                '&:hover': {
+                  backgroundColor: 'secondary.main'
+                }
+              }}>
+              <Typography variant="body2"
+                color="initial"
+                sx={{ ml: 1 }}
+              >
+                Salir
+              </Typography>
+            </Box>
+
+      </Stack>
+    </Paper>
   )
 }
-
-export default accountMenu
