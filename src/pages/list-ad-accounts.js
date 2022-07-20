92e931f895016c0b45d69d1a451f720ab43957d0
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-19 22:03:07 -0500

    layout basico de la pagina

--- src/pages/list-ad-accounts.js
+++ src/pages/list-ad-accounts.js
@@ -2,13 +2,25 @@
 
 import React from 'react'
 
+import { DashboardLayout } from '../components/dashboard-layout';
+
+
 /**
  * Esta página va a desplegar la lista de cuentas de anuncios que tiene el usuario contectadas
  * a la herramienta de Startop. Se despliega cuando se accede al apartado de "campañas" en el sidebar.
  * @returns 
  */
-const ListAdaccounts = () => {
-  return (
+const ListAdaccounts = () => (
+  <>
     <div>list-adccounts</div>
-  )
-}
+    </>
+);
+
+
+ListAdaccounts.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
+
+export default ListAdaccounts;
