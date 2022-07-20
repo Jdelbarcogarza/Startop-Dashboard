738fbd47349c6b3ab0cb0b9d07a7d727507da705
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-20 17:35:37 -0500

    se agregaron los elementosde la ventana de lista de ad accounts

--- src/pages/list-ad-accounts.js
+++ src/pages/list-ad-accounts.js
@@ -2,8 +2,13 @@
 
 import React from 'react'
 
+import { Box, Stack, Typography } from '@mui/material'
+import { AdAccountCard } from '../components/adAccount-card'
 import { DashboardLayout } from '../components/dashboard-layout';
 
+
+//FAKER
+import { faker } from '@faker-js/faker'
 
 /**
  * Esta página va a desplegar la lista de cuentas de anuncios que tiene el usuario contectadas
@@ -12,8 +17,44 @@ import { DashboardLayout } from '../components/dashboard-layout';
  */
 const ListAdaccounts = () => (
   <>
-    <div>list-adccounts</div>
-    </>
+  <Typography variant="body1" 
+  color="initial"
+  sx={{mt:2}}>
+    Seleccionar una cuenta de anuncios de la lista para ver sus campañas activas
+  </Typography>
+
+    <Box sx={{ px: 2, pr: 4, mt: 6, display: 'flex', justifyContent: 'center' }}>
+      <Stack gap={4}
+        sx={{ width: '60%' }}>
+        <AdAccountCard
+          name={faker.company.companyName()}
+          id={faker.random.alphaNumeric(12).toUpperCase()}
+        />
+        <AdAccountCard
+          name={faker.company.companyName()}
+          id={faker.random.alphaNumeric(12).toUpperCase()}
+        />
+        <AdAccountCard
+          name={faker.company.companyName()}
+          id={faker.random.alphaNumeric(12).toUpperCase()}
+        />
+        <AdAccountCard
+          name={faker.company.companyName()}
+          id={faker.random.alphaNumeric(12).toUpperCase()}
+        />
+        <AdAccountCard
+          name={faker.company.companyName()}
+          id={faker.random.alphaNumeric(12).toUpperCase()}
+        />
+        <AdAccountCard
+          name={faker.company.companyName()}
+          id={faker.random.alphaNumeric(12).toUpperCase()}
+        />
+      </Stack>
+
+
+    </Box>
+  </>
 );
 
 
