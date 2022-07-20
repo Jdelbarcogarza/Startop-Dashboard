b8dc126ff1bc4bb67a77ad2e3e9f74b26d8aa4b6
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-19 21:18:48 -0500

    componente de KPI

---
+++ src/components/dashboard/KPI-card.js
@@ -0,0 +1,43 @@
+
+import React from 'react'
+import {
+    Typography, Card, CardContent, Box, Avatar
+
+} from '@mui/material'
+
+
+
+export const KpiCard = ({ title, value, unit, icon }) => {
+    return (
+
+        // agregar una animacion de loading si no esta listo para desplegarse el valor
+
+        <Card sx={{ height: '100%' }}>
+
+            <CardContent sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography variant="overline"
+                        color="textSecondary">
+                        {title}
+                    </Typography>
+
+                    <Typography variant='h4'
+                        color='textPrimary'>
+                        {value}
+                    </Typography>
+
+                    <Typography variant="caption"
+                        color="textSecondary"
+                        sx={{ mt: 2 }}>
+                        {unit}
+                    </Typography>
+                </Box>
+
+                <Avatar sx={{width: 48, height: 48, backgroundColor: 'primary.main',}}>
+                    {icon}
+                </Avatar>
+            </CardContent>
+        </Card>
+    )
+}
