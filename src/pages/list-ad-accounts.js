e9145c2c2716dc3222af7095f29869127fd26855
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-19 21:54:18 -0500

    pagina de lista de ad accounts

---
+++ src/pages/list-ad-accounts.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+/**
+ * Esta página va a desplegar la lista de cuentas de anuncios que tiene el usuario contectadas
+ * a la herramienta de Startop. Se despliega cuando se accede al apartado de "campañas" en el sidebar.
+ * @returns 
+ */
+const ListAdaccounts = () => {
+  return (
+    <div>list-adccounts</div>
+  )
+}
