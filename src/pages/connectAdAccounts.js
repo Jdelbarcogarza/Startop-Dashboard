f6745c469468bc557256586b48d37daf10d1b2f9
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-21 18:30:04 -0500

    se incorpor[o componente que contiene las ad account cards

--- src/pages/connectAdAccounts.js
+++ src/pages/connectAdAccounts.js
@@ -2,31 +2,29 @@
 import React from 'react'
 import { DashboardLayout } from 'src/components/dashboard-layout'
 import { ConnectAdAccountCard } from 'src/components/adAccountCard/connect-account-card'
-import { Box, Stack } from '@mui/material'
+import { AdAccountListLayout } from 'src/components/adAccount-list-layout';
+
 
 import { faker } from '@faker-js/faker'
 
 const connectAdAccounts = () => {
   return (
     <>
-      <Box sx={{ px: 2, pr: 4, mt: 6, display: 'flex', justifyContent: 'center' }}>
-        <Stack gap={4}
-          sx={{ width: '60%' }}>
 
-          <ConnectAdAccountCard name={faker.company.companyName()}
-            id={faker.random.alphaNumeric(12).toUpperCase()}
-          />
+      <AdAccountListLayout>
+        <ConnectAdAccountCard name={faker.company.companyName()}
+          id={faker.random.alphaNumeric(12).toUpperCase()}
+        />
 
-          <ConnectAdAccountCard name={faker.company.companyName()}
-            id={faker.random.alphaNumeric(12).toUpperCase()}
-          />
+        <ConnectAdAccountCard name={faker.company.companyName()}
+          id={faker.random.alphaNumeric(12).toUpperCase()}
+        />
 
-          <ConnectAdAccountCard name={faker.company.companyName()}
-            id={faker.random.alphaNumeric(12).toUpperCase()}
-          />
+        <ConnectAdAccountCard name={faker.company.companyName()}
+          id={faker.random.alphaNumeric(12).toUpperCase()}
+        />
 
-        </Stack>
-      </Box>
+      </AdAccountListLayout>
     </>
   )
 }
