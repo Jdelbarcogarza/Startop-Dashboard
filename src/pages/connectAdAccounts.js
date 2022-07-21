4edc453fdc0af20af0271e0229b27eaccab3e38a
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-21 18:08:19 -0500

    ventana de conectar ad accounts

--- src/pages/connectAdAccounts.js
+++ src/pages/connectAdAccounts.js
@@ -1,17 +1,40 @@
 
 import React from 'react'
 import { DashboardLayout } from 'src/components/dashboard-layout'
+import { ConnectAdAccountCard } from 'src/components/adAccountCard/connect-account-card'
+import { Box, Stack } from '@mui/material'
+
+import { faker } from '@faker-js/faker'
 
 const connectAdAccounts = () => {
   return (
-    <div>connectAdAccounts</div>
+    <>
+      <Box sx={{ px: 2, pr: 4, mt: 6, display: 'flex', justifyContent: 'center' }}>
+        <Stack gap={4}
+          sx={{ width: '60%' }}>
+
+          <ConnectAdAccountCard name={faker.company.name()}
+            id={faker.random.alphaNumeric(12).toUpperCase()}
+          />
+
+          <ConnectAdAccountCard name={faker.company.name()}
+            id={faker.random.alphaNumeric(12).toUpperCase()}
+          />
+
+          <ConnectAdAccountCard name={faker.company.name()}
+            id={faker.random.alphaNumeric(12).toUpperCase()}
+          />
+
+        </Stack>
+      </Box>
+    </>
   )
 }
 
 connectAdAccounts.getLayout = (page) => (
-    <DashboardLayout>
-        {page}
-    </DashboardLayout>
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
 )
 
 export default connectAdAccounts
