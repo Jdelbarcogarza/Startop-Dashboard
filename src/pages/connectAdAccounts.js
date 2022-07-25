86bb201742a69df6477c37df9fcd80d07f37c607
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-25 15:51:30 -0500

    reset

--- src/pages/connectAdAccounts.js
+++ src/pages/connectAdAccounts.js
@@ -6,41 +6,52 @@ import { AdAccountListLayout } from 'src/components/adAccount-list-layout';
 
 
 import { faker } from '@faker-js/faker'
-import { Box, Typography, Button } from '@mui/material'
+import { Box, Typography, Button, Container } from '@mui/material'
 import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
 
 const connectAdAccounts = () => {
   return (
     <>
 
-      <AdAccountListLayout>
 
-        <ConnectAdAccountCard name={faker.company.companyName()}
-          id={faker.random.alphaNumeric(12).toUpperCase()}
-        />
+      <Container sx={{ position: 'fixed', backgroundColor: 'background.default', zIndex: 2, }}>
+        <Typography variant="h5"
+          color="primary.text"
+          sx={{ textAlign: 'center', py: 2, }}>
+            Conectar cuentas de anuncios
+        </Typography>
+      </Container>
 
-        <ConnectAdAccountCard name={faker.company.companyName()}
-          id={faker.random.alphaNumeric(12).toUpperCase()}
-        />
+      <Box sx={{mt:8}}>
+        <AdAccountListLayout>
 
-        <ConnectAdAccountCard name={faker.company.companyName()}
-          id={faker.random.alphaNumeric(12).toUpperCase()}
-        />
+          <ConnectAdAccountCard name={faker.company.companyName()}
+            id={faker.random.alphaNumeric(12).toUpperCase()}
+          />
+      
+          <ConnectAdAccountCard name={faker.company.companyName()}
+            id={faker.random.alphaNumeric(12).toUpperCase()}
+          />
 
-        <ConnectAdAccountCard name={faker.company.companyName()}
-          id={faker.random.alphaNumeric(12).toUpperCase()}
-        />
+          <ConnectAdAccountCard name={faker.company.companyName()}
+            id={faker.random.alphaNumeric(12).toUpperCase()}
+          />
 
-        <ConnectAdAccountCard name={faker.company.companyName()}
-          id={faker.random.alphaNumeric(12).toUpperCase()}
-        />
-      </AdAccountListLayout>
+          <ConnectAdAccountCard name={faker.company.companyName()}
+            id={faker.random.alphaNumeric(12).toUpperCase()}
+          />
+
+          <ConnectAdAccountCard name={faker.company.companyName()}
+            id={faker.random.alphaNumeric(12).toUpperCase()}
+          />
+        </AdAccountListLayout>
+      </Box>
 
       {/** Boton que termina de registrar inicialmente las ad accounts en la plataforma */}
-      <Button variant="contained" 
-      endIcon={<ArrowForwardOutlinedIcon/>}
-      color="primary"
-      sx={{position: 'fixed', right: '5%', bottom: 64}}>
+      <Button variant="contained"
+        endIcon={<ArrowForwardOutlinedIcon />}
+        color="primary"
+        sx={{ position: 'fixed', right: '5%', bottom: 64 }}>
         Terminar
       </Button>
 
