02a7476ccda37989c79965ac90a513ff7a9e9ca5
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-26 17:10:27 -0500

    formik form intergrada

--- src/components/settings/settings-password.js
+++ src/components/settings/settings-password.js
@@ -1,11 +1,34 @@
 import { useState } from 'react';
 import { Box, Button, Card, CardContent, CardHeader, Divider, TextField } from '@mui/material';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { formatRelative } from 'date-fns';
 
 export const SettingsPassword = (props) => {
   const [values, setValues] = useState({
+    currentPassword2: '',
     password: '',
     confirm: ''
   });
+
+  const formik = useFormik({
+    initialValues: {
+      currentPassword: '', // AQUI VA A IR EL CURRENT PASSWORD (esta lleno aqui)
+      newPassword: '',
+      confirmPassword: ''
+    },
+    validationSchema: Yup.object({
+      currentPassword: Yup
+        .string(),
+      newPassword: Yup
+        .string(),
+      confirmPassword: Yup
+        .string(),
+    }),
+    onSubmit: () => {
+      alert('Aqui se hace el cambio del password en la DB')
+    }
+  })
 
   const handleChange = (event) => {
     setValues({
@@ -15,7 +38,8 @@ export const SettingsPassword = (props) => {
   };
 
   return (
-    <form {...props}>
+    <form {...props}
+    onSubmit={formik.handleSubmit}>
       <Card>
         <CardHeader
           subheader="Update password"
@@ -23,24 +47,34 @@ export const SettingsPassword = (props) => {
         />
         <Divider />
         <CardContent>
-          <TextField
+        <TextField
             fullWidth
-            label="Password"
+            label="Contraseña actual"
             margin="normal"
-            name="password"
-            onChange={handleChange}
+            name="currentPassword"
+            onChange={formik.handleChange}
             type="password"
-            value={values.password}
+            value={formik.values.currentPassword}
             variant="outlined"
           />
           <TextField
             fullWidth
-            label="Confirm password"
+            label="Nueva contraseña"
             margin="normal"
-            name="confirm"
-            onChange={handleChange}
+            name="newPassword"
+            onChange={formik.handleChange}
             type="password"
-            value={values.confirm}
+            value={formik.values.newPassword}
+            variant="outlined"
+          />
+          <TextField
+            fullWidth
+            label="Reescribir nueva contraseña"
+            margin="normal"
+            name="confirmPassword"
+            onChange={formik.handleChange}
+            type="password"
+            value={formik.values.confirmPassword}
             variant="outlined"
           />
         </CardContent>
@@ -55,8 +89,14 @@ export const SettingsPassword = (props) => {
           <Button
             color="primary"
             variant="contained"
-          >
-            Update
+            type="submit"
+            
+          > 
+          {/** disabled={formik.isSubmitting} 
+            LUEGO HACER QUE SOLO SE PERMITA HACER CAMBIOS DE CONTRASEÑA CADA DOS
+            DÍAS O ASÍ. AHI ENTRARIA ESTE PROP DEL COMPONENTE
+          */}
+            Guardar contraseña
           </Button>
         </Box>
       </Card>
