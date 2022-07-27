171cb9d9ffadc7e8fc9cf569dff7b9e3e0c2902b
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-27 18:50:47 -0500

    se comentó la sección de notificacionse

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -25,7 +25,7 @@ const Settings = () => (
         >
           Ajustes
         </Typography>
-        <SettingsNotifications />
+        {/*<SettingsNotifications />*/}
         <Box sx={{ pt: 3 }}>
           <SettingsPassword />
         </Box>
