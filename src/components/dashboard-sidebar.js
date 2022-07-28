2dc599d368bb9eff9887c54e487bdebeccd53800
Jorge Del Barco <jdelbarcogarza@gmail.com>
2022-07-27 19:16:54 -0500

    se quitaron links no necesarios

--- src/components/dashboard-sidebar.js
+++ src/components/dashboard-sidebar.js
@@ -34,46 +34,6 @@ const items = [
     icon: (<BarChartIcon fontSize="small" />),
     title: 'Campañas'
   },
-  {
-    href: '#',
-    icon: (<ChartBarIcon fontSize="small" />),
-    title: 'Dashboard'
-  },
-  {
-    href: '/customers',
-    icon: (<UsersIcon fontSize="small" />),
-    title: 'Customers'
-  },
-  {
-    href: '/products',
-    icon: (<ShoppingBagIcon fontSize="small" />),
-    title: 'Products'
-  },
-  {
-    href: '/account',
-    icon: (<UserIcon fontSize="small" />),
-    title: 'Account'
-  },
-  {
-    href: '/settings',
-    icon: (<CogIcon fontSize="small" />),
-    title: 'Settings'
-  },
-  {
-    href: '/login',
-    icon: (<LockIcon fontSize="small" />),
-    title: 'Login'
-  },
-  {
-    href: '/register',
-    icon: (<UserAddIcon fontSize="small" />),
-    title: 'Register'
-  },
-  {
-    href: '/404',
-    icon: (<XCircleIcon fontSize="small" />),
-    title: 'Error'
-  }
 ];
 
 
